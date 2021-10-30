a8877908a3a2a72083fc842cc4feabaead0c13bd
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2021-10-30 14:39:50 +0900

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,25 @@
+﻿# mod1.py
+PI = 3.141592
+
+def sum(num1, num2):
+	result = num1 + num2
+	return result
+
+class FourCal1:
+	def __init__(self, num1, num2): 
+		self.first = num1
+		self.second = num2
+		print("객체 생성시 자동으로 실행") # 추가
+	def add(self):
+		self.result = self.first + self.second
+		return self.result
+	def sub(self):
+		self.result = self.first - self.second
+		return self.result
+	def mul(self):
+		self.result = self.first * self.second
+		return self.result
+	def div(self):
+		self.result = self.first / self.second
+		return self.result
+dddd = FourCal1(30, 20)
